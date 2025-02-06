0b6d4687ae95b8c714df1c9a6d42338695e4ddac
caio-sdm <marinhocaio1308@gmail.com>
2025-02-05 22:49:16 -0300

    d009 + if

--- desafios/d009.py
+++ desafios/d009.py
@@ -1,13 +1,21 @@
 print('Tabuada')
-num = int(input('Digite um número: '))
-n1 = num * 1
-n2 = num * 2
-n3 = num * 3
-n4 = num * 4
-n5 = num * 5
-n6 = num * 6
-n7 = num * 7
-n8 = num * 8
-n9 = num * 9
-n10 = num * 10
-print('A tabudada do {} é:\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(num,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
+num = int(input('Digite um número para ver sua tabuada: '))
+print('A tabuada do {} é:'.format(num))
+if 99 >= num >= 10:
+    print('='*13)
+if num > 99:
+    print('='*15)
+print('{} x {:>2} = {}'.format(num, 1, num*1))
+print('{} x {:>2} = {}'.format(num, 2, num*2))
+print('{} x {:>2} = {}'.format(num, 3, num*3))
+print('{} x {:>2} = {}'.format(num, 4, num*4))
+print('{} x {:>2} = {}'.format(num, 5, num*5))
+print('{} x {:>2} = {}'.format(num, 6, num*6))
+print('{} x {:>2} = {}'.format(num, 7, num*7))
+print('{} x {:>2} = {}'.format(num, 8, num*8))
+print('{} x {:>2} = {}'.format(num, 9, num*9))
+print('{} x {:>2} = {}'.format(num, 10, num*10))
+if 99 >= num >= 10:
+    print('='*13)
+if num > 99:
+    print('='*15)
