6eaf7a7d621bac6f24e0ac86df0c79938b97ca1e
caio-sdm <marinhocaio1308@gmail.com>
2025-02-05 22:33:21 -0300

    d008

--- desafios/d008.py
+++ desafios/d008.py
@@ -6,4 +6,4 @@ m = m
 dm = m*10
 cm = m*100
 mm = m*1000
-print('\nEm quilômetros: {} \nEm hectâmetro: {} \nEm decâmetro: {} \nEm metros: {} \nEm decímetros: {} \nEm centímetros: {} \nEm milímetros: {}'.format(km,hm,dam,m,dm,cm,mm))
+print('\nEm quilômetros: {}km \nEm hectâmetro: {}hm \nEm decâmetro: {}dam \nEm metros: {}m \nEm decímetros: {}dm \nEm centímetros: {}cm \nEm milímetros: {}mm'.format(km,hm,dam,m,dm,cm,mm))
