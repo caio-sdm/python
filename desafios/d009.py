07c0310cd13b48c4df11b5c6d552b5476399ba85
caio-sdm <marinhocaio1308@gmail.com>
2025-02-06 10:31:48 -0300

    d009 e 010

--- desafios/d009.py
+++ desafios/d009.py
@@ -1,6 +1,8 @@
 print('Tabuada')
 num = int(input('Digite um número para ver sua tabuada: '))
 print('A tabuada do {} é:'.format(num))
+if num <10:
+    print('='*11)
 if 99 >= num >= 10:
     print('='*13)
 if num > 99:
@@ -15,6 +17,8 @@ print('{} x {:>2} = {}'.format(num, 7, num*7))
 print('{} x {:>2} = {}'.format(num, 8, num*8))
 print('{} x {:>2} = {}'.format(num, 9, num*9))
 print('{} x {:>2} = {}'.format(num, 10, num*10))
+if num <10:
+    print('='*11)
 if 99 >= num >= 10:
     print('='*13)
 if num > 99:
--- desafios/d010.py
+++ desafios/d010.py
@@ -1,4 +1,4 @@
 print('Calculadora de dólares')
 r = float(input('Digite o valor em reais: '))
 d = r / 5.77
-print('Com esse valor, você teria {:.2f} dólares'.format(d))
+print('Com R${:.2f}, você teria US${:.2f} dólares'.format(r,d))
