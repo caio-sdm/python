42e946efe8dd6ce52e5fb37b8623fa6cadcfdcbd
caio-sdm <marinhocaio1308@gmail.com>
2025-02-24 23:24:39 -0300

    médias

---
+++ ex alt/medias.py
@@ -0,0 +1,9 @@
+print('Calculadora de médias anuais (4 bimestres)')
+print('='*30)
+num1 = float(input('Nota do primeiro: '))
+num2 = float(input('Nota do primeiro: '))
+num3 = float(input('Nota do primeiro: '))
+num4 = float(input('Nota do primeiro: '))
+media = (num1 + num2 + num3 + num4)/4
+print('sua média é: {:.2f}'.format(media))
+print('='*30)
