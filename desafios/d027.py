4a9ba72521499c844487e9995e0da01d0a68008e
caio-sdm <marinhocaio1308@gmail.com>
2025-04-02 23:05:14 -0300

    d027

--- desafios/d027.py
+++ desafios/d027.py
@@ -1,3 +1,10 @@
 # programa que leia o nome completo de uma pessoa e mostre o primeiro e o último nome.
 # Ex: luis inacio da silva bolsonaro. primeiro nome: luis ; último nome: bolsonaro
 
+print('='*30)
+
+print('Seu primeiro e último nome')
+
+nome = str(input("Escreva seu nome completo: ")).title().strip()
+separa = nome.split()
+print('Seu primeiro nome é {} e seu último nome é {}'.format(separa[0], separa[-1]))
